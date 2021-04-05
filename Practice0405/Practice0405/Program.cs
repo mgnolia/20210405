4de3d113ed3efb770776f325a65d55caf840e5d8
mgnolia <tnqls6560@gmail.com>
2021-04-05 22:59:22 +0900

    .

--- Practice0405/Practice0405/Program.cs
+++ Practice0405/Practice0405/Program.cs
@@ -10,24 +10,28 @@ namespace Practice0405
     {
         static void Main(string[] args)
         {
+            //inch => cm 변환
+            Console.WriteLine("inch");
             double inch = double.Parse(Console.ReadLine());
             Console.WriteLine($"{inch*2.54}cm");
 
 
-
+            //kg => pound 변환
             double pound = double.Parse(Console.ReadLine());
             Console.WriteLine($"{pound*2.20462262}pound");
 
-
+            //원의 반지름을 입력받아서 원의 둘레와 넓이 구하기
             double Ban = double.Parse(Console.ReadLine());
             const double PI = 3.14;//PI 상수(대문자가 국룰)
             Console.WriteLine("둘레 : "+2*PI*Ban);
             Console.WriteLine("넓이 : "+3.14*Ban*Ban);
-
+            //4번 472*385(사실 몇이랑 몇 곱하는 문제인지 기억안나서 정확하진 않아요)
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
+            string bb = b.ToString();
 
-            Console.WriteLine();
+            Console.WriteLine(a * int.Parse(bb[2].ToString()));
+            Console.WriteLine(a * int.Parse(bb[1].ToString()));
             Console.WriteLine(a*b);
 
         }
